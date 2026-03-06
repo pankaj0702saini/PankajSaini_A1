34a77590e996fac18bf63fc802a39a672e0f76d9
pankaj0702saini <pankajsainibuss02@gmail.com>
2026-03-06 08:44:38 +0530

    All sorting programm

--- sorting.c
+++ sorting.c
@@ -11,6 +11,7 @@ void display(int *arr, int size)
 // Programm of bubble sort
 void bubble_sort(int *arr, int size)
 {
+    printf("Running bubble sort...\n");
     for (int i = 0; i < size - 1; i++) // number of passes
     {   
         // int isSorted=0; 
@@ -28,11 +29,35 @@ void bubble_sort(int *arr, int size)
         //     return;
     }
 }
+
+// Programm of selection sort
+void selection_sort(int *arr,int n)
+{
+    printf("Running selection sort...\n");
+    int IndexOfMin,temp;
+    for(int i=0;i<n-1;i++)
+    {
+        IndexOfMin=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[IndexOfMin])
+            {
+                IndexOfMin=j;
+            }
+        }
+        // Swap
+        temp=arr[i];
+        arr[i]=arr[IndexOfMin];
+        arr[IndexOfMin]=temp;
+    }
+}
+
 int main()
 {
     int arr[] = {11, 3, 5, 15, 7, 19};
-    int n = 6;
+    int n = sizeof(arr)/sizeof(arr[0]);
     display(arr, n);
-    bubble_sort(arr, n); // Program of bubble sort array
+    // bubble_sort(arr, n); // Program of bubble sort array
+    selection_sort(arr,n);  // Program of selection sort array
     display(arr, n);  // sorted array
 }
